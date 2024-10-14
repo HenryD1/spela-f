7920585dc54a907c7fc941e055aa01ccea038541
Henry Dowling <htdowling@gmail.com>
2024-10-14 10:09:27 -0700

    temp eod 1013

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,6 +8,14 @@ import logo from '../assets/volley-logo.png';
 const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
 const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize';
 
+const categories = [
+  { name: 'Animals'}, 
+  { name: 'Wallpapers'}, 
+  { name: 'Photography'}, 
+  { name: 'Gaming'}, 
+  { name: 'Coding'},
+  { name: 'Other'},  
+]
 
 const Sidebar = ( user, closeToggle ) => {
 
@@ -33,6 +41,8 @@ const Sidebar = ( user, closeToggle ) => {
           <RiHomeFill />
           Home
         </NavLink>
+        <h3 className="mt-2 px-5 text-base 2xl:text-xl"> Discover categories</h3>
+
      </div>
      </div>
      
