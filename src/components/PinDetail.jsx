1d2e00eb5f4cb079ea412b8bfe1d34aa16106f81
Henry Dowling <htdowling@gmail.com>
2024-10-17 13:34:37 -0700

    temp 1017

---
+++ src/components/PinDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PinDetail = () => {
+  return (
+    <div>
+      Pin detail wow
+    </div>
+  )
+}
+
+export default PinDetail
--- src/container/Pins.jsx
+++ src/container/Pins.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState} from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import {Navbar, Feed, PinDetail, CreatePin, Search};
+
 
 const Pins = () => {
+  const[Seas]
   return (
-    <div>
-      hello im a pin
+    <div className="px-2 md:px-5">
+      <div className= "bg-gray-50">
+        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
     </div>
   )
 }
